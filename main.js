01836877c3b676e38ef4dad79193a025e39b69d4
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-02-10 11:35:12 +0200

    added the base map visualization to map-canvas

--- main.js
+++ main.js
@@ -5,7 +5,64 @@ var initialize = async function (url) {
   await getData(`${url}/location_data.json`, 'json', organizeData);
   await getData(`${url}/location_info.txt`, 'text', organizeInfo);
 
-  console.log(cache);
+  plotMap();
+  /*var url = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+  var svg = d3.select('#map-svg');
+  d3.json(url, function(error, topology) {
+    if (error) throw error;
+
+    console.log("topojson", topology)
+    var geojson = topojson.feature(topology, topology.objects.countries);
+    console.log(geojson);
+
+    svg.selectAll('path')
+      .data(geojson.features)
+      .enter()
+      .append('path')
+      .attr('d', d3.geo.path());
+  });*/
+}
+
+// Create the initial map view
+var plotMap = function () {
+  let svg = d3.select('#map-svg');
+  let boundingBox = d3.select('#map-canvas').node().getBoundingClientRect();
+  let width = boundingBox.width;
+  let height = boundingBox.height;
+
+  // let long = d3.scaleLinear()
+  //             .domain([0, width])
+  //             .range([-180, 180]);
+
+  // let lat = d3.scaleLinear()
+  //             .domain([0, height])
+  //             .range([90, -90]);
+
+  // svg.on('mousemove', function() {
+  //   var p = d3.mouse(this);
+  //   projection.rotate([λ(p[0]), φ(p[1])]);
+  //   svg.selectAll('path').attr('d', path);
+  // });
+
+  d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json")
+    .then(function (topology) {
+      // We first need to create a geojson from the topojson we are loading in.
+      let geojson = topojson.feature(topology, topology.objects.countries)
+
+      let projection = d3.geoOrthographic()
+                    .rotate([0, -90])
+                    .fitSize([width, height], geojson)
+                    .precision(.1);
+
+      let path = d3.geoPath()
+                  .projection(projection);
+
+      // Add the geojson features to the map SVG, using the path generator.
+      svg.selectAll('path')
+        .data(geojson.features) // We only want the features from the geojson
+        .enter().append('path')
+        .attr('d', path);
+    });
 }
 
 // getData reads json and text data from a local source to
@@ -14,7 +71,7 @@ var getData = async function (url, methodFunc, callback) {
   let response = await fetch(url);
   let resolve = await response[methodFunc]();
   callback(resolve);
-}
+};
 
 // organizeData gets the available age and value axis pairs
 // from location_data.json for all sediment cores.
